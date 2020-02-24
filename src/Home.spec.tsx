dfa0105fad043343b643358a6b924f05a42097e8
Tomasz Klęk <tklek@icloud.com>
2020-02-24 21:45:20 +0100

    Add describe functions

--- src/Home.spec.tsx
+++ src/Home.spec.tsx
@@ -6,33 +6,35 @@ import Home from './Home'
 import { initialState } from './App'
 import { TextField } from '@material-ui/core'
 
-it('renders correct structure', () => {
-  const home = shallow(<Home state={initialState} onStateChange={identity} />)
-  expect(home).toMatchSnapshot()
-})
-
-it('should call state update on beer name change', () => {
-  const stateChangeSpy = spy()
-  const home = shallow(<Home state={initialState} onStateChange={stateChangeSpy} />)
-
-  home.find(TextField).at(0).simulate('change', {
-    target: {
-      value: 'value'
-    }
+describe('Home', () => {
+  it('renders correct structure', () => {
+    const home = shallow(<Home state={initialState} onStateChange={identity} />)
+    expect(home).toMatchSnapshot()
   })
 
-  stateChangeSpy.calledWith({ filter: { name: 'value' }, page: 0 })
-})
+  it('should call state update on beer name change', () => {
+    const stateChangeSpy = spy()
+    const home = shallow(<Home state={initialState} onStateChange={stateChangeSpy} />)
 
-it('should call state update on malt change', () => {
-  const stateChangeSpy = spy()
-  const home = shallow(<Home state={initialState} onStateChange={stateChangeSpy} />)
+    home.find(TextField).at(0).simulate('change', {
+      target: {
+        value: 'value'
+      }
+    })
 
-  home.find(TextField).at(0).simulate('change', {
-    target: {
-      value: 'value'
-    }
+    stateChangeSpy.calledWith({ filter: { name: 'value' }, page: 0 })
   })
 
-  stateChangeSpy.calledWith({ filter: { malt: 'value' }, page: 0 })
+  it('should call state update on malt change', () => {
+    const stateChangeSpy = spy()
+    const home = shallow(<Home state={initialState} onStateChange={stateChangeSpy} />)
+
+    home.find(TextField).at(0).simulate('change', {
+      target: {
+        value: 'value'
+      }
+    })
+
+    stateChangeSpy.calledWith({ filter: { malt: 'value' }, page: 0 })
+  })
 })
--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,45 +1,47 @@
 import sinon from 'sinon'
 import callApi, { RequestMethod, API_URL } from './api'
 
-const fakeData = ['fake_beer']
+describe('api', () => {
+  const fakeData = ['fake_beer']
 
-const mockApiResponse = (body = {}, status: number) =>
-  Promise.resolve(new window.Response(JSON.stringify(body), {
-    status,
-    headers: { 'Content-type': 'application/json' }
-  }))
+  const mockApiResponse = (body = {}, status: number) =>
+    Promise.resolve(new window.Response(JSON.stringify(body), {
+      status,
+      headers: { 'Content-type': 'application/json' }
+    }))
 
-it('should resolve promise with data', async () => {
-  const stubedFetchSuccess = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 200))
-  const response = await callApi('endpoint', RequestMethod.GET, {})
-  expect(response).toEqual(fakeData)
-  expect(stubedFetchSuccess.calledOnce)
-  stubedFetchSuccess.restore()
-})
+  it('should resolve promise with data', async () => {
+    const stubedFetchSuccess = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 200))
+    const response = await callApi('endpoint', RequestMethod.GET, {})
+    expect(response).toEqual(fakeData)
+    expect(stubedFetchSuccess.calledOnce)
+    stubedFetchSuccess.restore()
+  })
 
-it('should reject promise and return response status', async () => {
-  const stubedFetchFailure = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 500))
-  try {
-    await callApi('endpoint', RequestMethod.GET, {})
-  } catch (responseStatus) {
-    expect(responseStatus).toEqual(500)
-    expect(stubedFetchFailure.calledOnce)
-  }
-  stubedFetchFailure.restore()
-})
-
-it('should call fetch with correct params', async () => {
-  const stubedFetch = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 200))
-  const fakeEndpoint = 'endpoint'
-  await callApi(fakeEndpoint, RequestMethod.GET, {})
-  expect(stubedFetch.calledWith(
-    `${API_URL}/${fakeEndpoint}`,
-    {
-      method: RequestMethod.GET,
-      body: JSON.stringify({}),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  it('should reject promise and return response status', async () => {
+    const stubedFetchFailure = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 500))
+    try {
+      await callApi('endpoint', RequestMethod.GET, {})
+    } catch (responseStatus) {
+      expect(responseStatus).toEqual(500)
+      expect(stubedFetchFailure.calledOnce)
     }
-  ))
+    stubedFetchFailure.restore()
+  })
+
+  it('should call fetch with correct params', async () => {
+    const stubedFetch = sinon.stub(window, 'fetch').returns(mockApiResponse(fakeData, 200))
+    const fakeEndpoint = 'endpoint'
+    await callApi(fakeEndpoint, RequestMethod.GET, {})
+    expect(stubedFetch.calledWith(
+      `${API_URL}/${fakeEndpoint}`,
+      {
+        method: RequestMethod.GET,
+        body: JSON.stringify({}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ))
+  })
 })
